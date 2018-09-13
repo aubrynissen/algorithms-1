e2488c74b4818ec0c6682b6e25bcfd58f2b65e07
Aubry <aubrynissen@gmail.com>
2018-09-12 22:16:07 -0500

    Removed multidimensional arrays and implemented quick-find first

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -4,13 +4,11 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdRandom;
+
 public class Percolation {
 
-    /*    private boolean[][] openFromCoord;
-        private boolean[] openFromNum;
-        private int[][] idFromCoord;
-        private int[] idFromNum;*/
-    private boolean[] open;
+    private boolean[] open1;
     private int[] id;
     private int n;
     private int topsite = -1;
@@ -20,92 +18,126 @@ public class Percolation {
     {
         int numSites = n * n;
         id = new int[numSites];
-        open = new boolean[numSites];
+        open1 = new boolean[numSites];
 
         for (int i = 0; i < numSites;
-             i++)            // loop through rows
+             i++)            // loop through sites
         {
-
-            updateID(i, i);
-            updateOpenFromInt(i, false);
+            if (i < n) id[i] = -1;
+            else {
+                id[i] = i;
+            }
+            open1[i] = false;
+            System.out.println(
+                    "i: " + Integer.toString(i) + " | row: " + Integer
+                            .toString(((i - (i % n)) / n) + 1)
+                            + " | column: " + Integer.toString((i % n) + 1));
         }
+    }
+
+    private int iFromCoord(int row, int col) {
+
+        int i = (((row - 1) * this.n) + (col - 1));
+
+        return i;
+
+    }
+
+    private int colFromI(int i) {
+
+        int col = (i % this.n) + 1;
+
+        return col;
+
+    }
+
+    private int rowFromI(int i) {
+
+        int row = ((i - ((i % this.n) - 1)) / this.n) + 1;
+
+        return row;
+
     }
 
 
     private int root(int row,
-                     int col)     //return root and update roots along the way (path compression)
+                     int col)     // return root and update roots along the way (path compression)
     {
-        int i = (((row - 1) * this.n) + col);
+        int i = iFromCoord(row, col);
         int newID;
 
-        while ((i != id[i]) && (id[i] != topsite)) {
+        while ((i != id[i]) && (topsite != id[i])) {
 
-            newID = idFromNum[idFromNum[i]];
-            updateID(i, newID);
+            newID = id[id[i]];
+            id[i] = newID;
             i = newID;
 
         }
         return i;
     }
 
-    private void updateID(int i, int newID) {
-
-        int col = (i % this.n) + 1;
-        int row = (i - col) / this.n;
-
-        idFromNum[i] = newID;
-        idFromCoord[row][col] = newID;
-
-
-    }
-
-    private void updateOpenFromInt(int i, boolean o) {
-
-        int col = (i % this.n) + 1;
-        int row = (i - col) / this.n;
-
-        openFromNum[i] = true;
-        openFromCoord[row][col] = true;
-
-        // now that we've opened that site, see if any of the 4 adjacent sites are open. for each that is, set the id of all of them equal to the lowest number
-
-
-    }
-
-    private void setOpenFromInt(int i) {
-
-        int col = (i % this.n) + 1;
-        int row = (i - col) / this.n;
-
-        openFromNum[i] = true;
-        openFromCoord[row][col] = true;
-
-        // now that we've opened that site, see if any of the 4 adjacent sites are open. for each that is, set the id of all of them equal to the lowest number
-
-
-    }
-
-    private void setOpenFromCoord(int row, int col) {
-
-        int i = (this.n * (row - 1)) + (col - 1);
-
-        openFromNum[i] = true;
-        openFromCoord[row][col] = true;
-
-        //now that we've opened that site, see if any of the 4 adjacent sites are open. for each that is, set the id of all of them equal to the lowest number
-
-
-    }
 
     public void open(int row, int col)    // open site (row, col) if it is not open already
     {
-        updateOpen();
+        int i = iFromCoord(row, col);
+
+        open1[i] = true;
+
+        // now that we've opened that site, see if any of the 4 adjacent sites are open. for each that is, join the components by calling union
+
+        for (int x = -1; x < 1; x += 2) {
+
+            int r = row + x;
+
+            if (isOpen(r, col)) union(row, col, r, col);
+
+        }
+
+        for (int x = -1; x < 1; x += 2) {
+
+            int c = col + x;
+
+            if (isOpen(row, c)) union(row, col, row, c);
+
+        }
+    }
+
+    private void union(int site1Row, int site1Col, int site2Row, int site2Col) {
+
+
+        int site1i = iFromCoord(site1Row, site1Col);
+        int site2i = iFromCoord(site2Row, site2Col);
+
+        int site1id = id[site1i];
+        int site2id = id[site2i];
+
+        int minID;
+        int maxID;
+
+        if (site1id
+                < site2id)  // find minimum id for root to set id values of component sites to (because -1 is the minimum and we want it to spread)
+        {
+            minID = site1id;
+            maxID = site2id;
+        }
+        else {
+            minID = site2id;
+            maxID = site1id;
+        }
+
+        for (int i = 0; i < id.length; i++) {
+
+            if (id[i] == maxID) id[i] = minID;
+
+        }
+
 
     }
 
     public boolean isOpen(int row, int col)  // is site (row, col) open?
     {
-        return openFromCoord[row][col];
+        int i = iFromCoord(row, col);
+        return open1[i];
     }
 
     public boolean isFull(int row, int col)  // is site (row, col) full?
@@ -120,31 +152,62 @@ public class Percolation {
 
     public int numberOfOpenSites()       // number of open sites
     {
+        int openSites = 0;
 
+        for (int i = 0; i < open1.length; i++) {
+
+            if (open1[i]) openSites++;
+
+        }
+
+        return openSites;
     }
 
     public boolean percolates()              // does the system percolate?
     {
-        int x = this.n;
+        int x = id.length;
+        boolean p = false;
+        int lastRow = this.n;
+        int firstCol = 1;
+        int lastRowFirstCol = iFromCoord(lastRow, firstCol);
 
-        for (int i = 0; i < x; i++) {
-            if (id[x - 1][i] == -1) {
-                return true;
-            }
-            else {
-                return false;
+        for (int i = lastRowFirstCol; i < x; i++) {
+            if (id[i] == -1) {
+                p = true;
+                break;
             }
         }
+
+        return p;
     }
 
     public static void main(String[] args) {
 
-        int n = Integer.parseInt(args[0]);
+        int N = Integer.parseInt(args[0]);
 
-        Percolation p = new Percolation(n);
+        Percolation p = new Percolation(N);
 
-        p.n = n;
+        p.n = N;
 
+        while (!p.percolates()) {
 
+            int randomSite = StdRandom.uniform(N);
+
+            int row = p.rowFromI(randomSite);
+            int col = p.colFromI(randomSite);
+            System.out.println(
+                    "Identified site " + Integer.toString(randomSite) + " at coodinates (" + Integer
+                            .toString(row) + ", " + Integer.toString(col) + ")");
+
+            p.open(row, col);
+            System.out.println(
+                    "Opened site " + Integer.toString(randomSite) + " at coodinates (" + Integer
+                            .toString(row) + ", " + Integer.toString(col) + ")");
+
+        }
+
+        int nos = p.numberOfOpenSites();
+
+        System.out.println(Integer.toString(nos));
     }
 }
